f99d0c2624a199598b9b3968594937ea33b9d516
Muse Tesfaye <tesfaye.musee@gmail.com>
2024-01-19 15:55:57 -0500

    switched to postgresql

--- dotpm/settings.py
+++ dotpm/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'dotpm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('RAILWAY_NAME'),
+        'USER': os.getenv('RAILWAY_USER'),
+        'PASSWORD': os.getenv('RAILWAY_PASSWORD'),
+        'HOST': os.getenv('RAILWAY_HOST'),
+        'PORT': os.getenv('RAILWAY_PORT')
     }
 }
 
